3592365c5e95acc5fa8219052101a061efa90206
justSmil3 <christian.lott@protonmail.com>
2021-12-17 00:38:49 +0100

    server push

--- pjapp/settings.py
+++ pjapp/settings.py
@@ -88,7 +88,7 @@ DATABASES={
       'ENGINE':'django.db.backends.postgresql_psycopg2',
       'NAME':'postgres',
       'USER':'postgres',
-      'PASSWORD':'VJ-F!U4_K;d&6\'P)',
+      "PASSWORD":"VJ-F!U4_K;d&6'P)",
       'HOST':'localhost',
       'PORT':'5432',
    }
