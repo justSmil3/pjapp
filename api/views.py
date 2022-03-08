84742000b8a69b3553c9a3536fe40ed605d0c017
justSmil3 <christian.lott@protonmail.com>
2022-03-08 21:46:19 +0100

    fixed track return

--- api/views.py
+++ api/views.py
@@ -265,7 +265,7 @@ def createTrack(request):
     try:
         track = Track.objects.get(task=task)
         serializer = TrackSerializer(track, data=request.data)
-        print("stupid")
+        print(serializer.errors)
         if serializer.is_valid():
             print("hi")
             serializer.save()
