6675551af08645ecdb01652ea1d7d0080c34e1a7
justSmil3 <christian.lott@protonmail.com>
2022-04-19 18:40:46 +0200

    change classes

--- api/views.py
+++ api/views.py
@@ -232,7 +232,7 @@ def getSubtasks(request):
             user=request.user, 
         )
         
-    # print(ed.get_abteil_display())
+    print(ed.get_abteil_display())
     subtasks = SubTask.objects.all()#.filter(classes__icontains="ALL")
     # userclass = ed.get_abteil_display()
     
