4e67d92b17839fc7cd0b48597e1584023b63b894
justSmil3 <christian.lott@protonmail.com>
2022-09-28 12:20:34 +0200

    test 1

--- api/views.py
+++ api/views.py
@@ -250,11 +250,12 @@ def getMentiSubtasks(request, pk):
             user=User.objects.get(id=pk), 
         )
         
+        
     # print(ed.get_abteil_display())
     subtasks = SubTask.objects.all().filter(classes=1)
     userclass = ed.abteil
     
-    subtasks = subtasks | SubTask.objects.all().filter(classes = userclass)
+    # subtasks = subtasks | SubTask.objects.all().filter(classes = userclass)
     subtasks = subtasks.order_by("task")
     serializer = SubtaskSerializer(subtasks, many=True)
     return Response(serializer.data)
